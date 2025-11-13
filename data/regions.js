81c5ad67f13f5633e0fcd150917670868e6bc5b4
yaxyobekuz <pubgn9642@gmail.com>
2025-11-13 19:19:04 +0500

    add regions data for Uzbekistan with multilingual support

---
+++ data/regions.js
@@ -0,0 +1,107 @@
+/**
+ * Regions of Uzbekistan
+ * Each region has translations in all supported languages
+ */
+
+const regions = [
+  {
+    id: "tashkent_city",
+    uz_latn: "Toshkent shahri",
+    uz_cyrl: "Тошкент шаҳри",
+    en: "Tashkent City",
+    ru: "г. Ташкент",
+  },
+  {
+    id: "tashkent",
+    uz_latn: "Toshkent viloyati",
+    uz_cyrl: "Тошкент вилояти",
+    en: "Tashkent Region",
+    ru: "Ташкентская область",
+  },
+  {
+    id: "andijan",
+    uz_latn: "Andijon viloyati",
+    uz_cyrl: "Андижон вилояти",
+    en: "Andijan Region",
+    ru: "Андижанская область",
+  },
+  {
+    id: "bukhara",
+    uz_latn: "Buxoro viloyati",
+    uz_cyrl: "Бухоро вилояти",
+    en: "Bukhara Region",
+    ru: "Бухарская область",
+  },
+  {
+    id: "fergana",
+    uz_latn: "Farg'ona viloyati",
+    uz_cyrl: "Фарғона вилояти",
+    en: "Fergana Region",
+    ru: "Ферганская область",
+  },
+  {
+    id: "jizzakh",
+    uz_latn: "Jizzax viloyati",
+    uz_cyrl: "Жиззах вилояти",
+    en: "Jizzakh Region",
+    ru: "Джизакская область",
+  },
+  {
+    id: "namangan",
+    uz_latn: "Namangan viloyati",
+    uz_cyrl: "Наманган вилояти",
+    en: "Namangan Region",
+    ru: "Наманганская область",
+  },
+  {
+    id: "navoi",
+    uz_latn: "Navoiy viloyati",
+    uz_cyrl: "Навоий вилояти",
+    en: "Navoi Region",
+    ru: "Навоийская область",
+  },
+  {
+    id: "kashkadarya",
+    uz_latn: "Qashqadaryo viloyati",
+    uz_cyrl: "Қашқадарё вилояти",
+    en: "Kashkadarya Region",
+    ru: "Кашкадарьинская область",
+  },
+  {
+    id: "karakalpakstan",
+    uz_latn: "Qoraqalpog'iston Respublikasi",
+    uz_cyrl: "Қорақалпоғистон Республикаси",
+    en: "Republic of Karakalpakstan",
+    ru: "Республика Каракалпакстан",
+  },
+  {
+    id: "samarkand",
+    uz_latn: "Samarqand viloyati",
+    uz_cyrl: "Самарқанд вилояти",
+    en: "Samarkand Region",
+    ru: "Самаркандская область",
+  },
+  {
+    id: "sirdarya",
+    uz_latn: "Sirdaryo viloyati",
+    uz_cyrl: "Сирдарё вилояти",
+    en: "Sirdarya Region",
+    ru: "Сырдарьинская область",
+  },
+  {
+    id: "surkhandarya",
+    uz_latn: "Surxondaryo viloyati",
+    uz_cyrl: "Сурхондарё вилояти",
+    en: "Surkhandarya Region",
+    ru: "Сурхандарьинская область",
+  },
+  {
+    id: "khorezm",
+    uz_latn: "Xorazm viloyati",
+    uz_cyrl: "Хоразм вилояти",
+    en: "Khorezm Region",
+    ru: "Хорезмская область",
+  },
+];
+
+module.exports = regions;
